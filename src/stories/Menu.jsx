eccd38fcfaab2a9b8f298364564c2347e0b62b5c
Jake Hallam <jake.hallam@havas.com>
2021-06-25 15:35:50 +0100

    adds local config

--- src/stories/Menu.jsx
+++ src/stories/Menu.jsx
@@ -2,23 +2,43 @@ import styled from 'styled-components'
 
 const MenuWrapper = styled.div`
   background-color: ${props => props.backgroundColor};
-  border-left: ${props => props.borderLeft};
   color: ${props => props.color};
   height: auto;
   font-family: sans-serif;
   font-size: 18px;
   font-weight: bold;
-  padding: 8px 0 8px 16px;
   text-transform: uppercase;
+
+  .dropdown-item {
+    border-top: 1px solid #CCC;
+    border-left: ${props => props.borderLeft};
+    margin-block-start: 0;
+    margin-block-end: 0;
+    padding: 1em 0 1em 16px;
+  }
 `
 
 export const Menu = ({ backgroundColor, borderLeft, color }) => {
+  const MENU_CONFIG = {
+    menuItems: [
+      { title: 'MENU ITEM 1' },
+      { title: 'MENU ITEM 2' },
+      { title: 'MENU ITEM 3' },
+    ]
+  }
+
   return (
     <MenuWrapper 
       backgroundColor={backgroundColor}
       borderLeft={borderLeft}
       color={color}>
-      <p>Menu Item 1</p>
+
+      {MENU_CONFIG.menuItems.map((item) => {
+        return(
+          <p className="dropdown-item">{item.title}</p>
+        )
+      })}
+
     </MenuWrapper>
   )
 }
